5e03250ce71f7233c7eabe24c48ea5f972077421
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-27 03:06:09 +0700

    Update tradingview.tsx

--- src/utils/tradingview.tsx
+++ src/utils/tradingview.tsx
@@ -1,17 +1,6 @@
 import { USE_MARKETS } from './markets';
 
 export const findTVMarketFromAddress = (marketAddressString: string) => {
-  // USE_MARKETS.forEach((market) => {
-  //   if (market.address.toBase58() === marketAddressString) {
-  //     return market.name;
-  //   }
-  // });
-  let market;
-  for (let i = 0; i < USE_MARKETS.length; i++) {
-    market = USE_MARKETS[i];
-    if (market.address.toBase58() === marketAddressString) {
-      return market.name;
-    }
-  }
-  return 'SRM/USDC';
+  const foundMarket = USE_MARKETS.find((market) => market.address.toBase58() === marketAddressString);
+  return foundMarket ? foundMarket.name : 'SRM/USDC';
 };
